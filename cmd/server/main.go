31c628babf24c1324f9e14c9ca0b41b67d4199a6
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-08-13 15:20:14 +0200

    stomp extention to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	url := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	defer connection.Close()
+	fmt.Println("Connection was successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println(" Connection was closed, shuting down")
 }
