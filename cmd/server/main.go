38373d49691caf78be405d1fe77f4ff01420a0fb
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-08-14 16:03:47 +0200

    pubsub package for PublishJSON

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +21,18 @@ func main() {
 	}
 	defer connection.Close()
 	fmt.Println("Connection was successful")
+
+	newChan, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Error creating channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(newChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("Error publishing JSON: %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
